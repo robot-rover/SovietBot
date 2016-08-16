a8c487116d2650d6cef7f9f22f3935adbbba5ba2
robot-rover <sam.obrien00@gmail.com>
2016-08-16 00:06:13 -0500

    Version 1.1.4 Fixed invite link in source code

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,18 +1,19 @@
 /*
 Add This Bot to Your Server:
-https://discordapp.com/oauth2/authorize?&client_id=184445488093724672&scope=bot&permissions=19934240
+https://discordapp.com/oauth2/authorize?&client_id=184445488093724672&scope=bot&permissions=19950624
  */
 
  /* Permissions List
 MANAGE_GUILD	0x00000020  Allows management and editing of the guild
 READ_MESSAGES	0x00000400  Allows reading messages in a channel. The channel will not appear for users without this permission
 SEND_MESSAGES	0x00000800  Allows for sending messages in a channel
-CONNECT 	0x00100000  Allows for joining of a voice channel
+EMBED_LINKS     0x00004000  Links sent by this user will be auto-embedded
+CONNECT 	    0x00100000  Allows for joining of a voice channel
 SPEAK           0x00200000  Allows for speaking in a voice channel
 MOVE_MEMBERS	0x01000000  Allows for moving of members between voice channels
 MANAGE_MESSAGES 0x00002000  Allows for deletion of other users messages
 
-Total:          19934240
+Total:          19950624
  */
 package main;
 
