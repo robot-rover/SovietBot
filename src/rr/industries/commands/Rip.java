3c960ff28169ce33bd94de0c924ab33ad75cc320
robot-rover <sam.obrien00@gmail.com>
2016-09-27 20:24:15 -0500

    Improved Rip Regex

--- src/rr/industries/commands/Rip.java
+++ src/rr/industries/commands/Rip.java
@@ -21,16 +21,12 @@ public class Rip implements Command {
             @Syntax(helpText = "Specify the thing(s) that are rip (Works with @\u200Bmentions)", args = {Arguments.TEXT})
     })
     public void execute(CommContext cont) {
-        String channelName;
-        Pattern p = Pattern.compile("<@!?[0-9]{18}>");
-        Pattern n = Pattern.compile("[0-9]{18}");
+        Pattern p = Pattern.compile("<@!?([0-9]{18})>");
         MessageBuilder message = new MessageBuilder(cont.getClient()).withChannel(cont.getMessage().getChannel()).withContent("http://www.ripme.xyz/");
         String rawSubject = cont.getConcatArgs(1);
         Matcher matcher = p.matcher(rawSubject);
         while (matcher.find()) {
-            Matcher num = n.matcher(matcher.group());
-            num.find();
-            String id = num.group();
+            String id = matcher.group(1);
             IUser mention = cont.getClient().getUserByID(id);
             if (mention != null) {
                 rawSubject = rawSubject.replace(matcher.group(), mention.getDisplayName(cont.getMessage().getGuild()));
@@ -38,8 +34,7 @@ public class Rip implements Command {
                 rawSubject = rawSubject.replace(matcher.group(), "");
             }
         }
-        rawSubject = rawSubject.replace(" ", "%20");
-        cont.getActions().channels().sendMessage(message.appendContent(rawSubject));
+        cont.getActions().channels().sendMessage(message.appendContent(rawSubject.replace(" ", "%20")));
     }
 
     @Override
