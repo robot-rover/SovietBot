a607b9172a8ba40da1419f0b6dd956d4cbd80470
robot_rover <sam.obrien00@gmail.com>
2017-12-13 16:54:19 -0600

    Fixed travis script build path

--- travisDeploy.sh
+++ travisDeploy.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+if [ ! -f $TRAVIS_BUILD_DIR/target/sovietBot-master.jar ]
+then
+    echo Built Jar does not exist!
+fi
+
+echo Uploading Jar
 sshpass -e sftp root@$SERVER << !
     rm /root/sovietBot/sovietBot-update.jar
-    put target/sovietBot-master.jar /root/sovietBot/sovietBot-update.jar
+    put $TRAVIS_BUILD_DIR/target/sovietBot-master.jar /root/sovietBot/sovietBot-update.jar
     bye
 !
+echo Sending Update POST
 curl -X POST --data "{\"command\": \"restart\",\"secret\": \"$SECRET\",\"name\": \"`md5sum target/sovietBot-master.jar | awk '{ print $1 }'`\"}" --header "Content-Type:application/json" https://www.sovietbot.xyz:2053/command
 echo ""
